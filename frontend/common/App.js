5e0e0e63829079d0016a9a6b2879aca038cbdd5a
sean <sean@koji-projects.com>
2020-04-09 18:00:25 +0000

    Manual deploy

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -3,25 +3,68 @@ import styled from 'styled-components';
 import Koji from '@withkoji/vcc';
 
 const Container = styled.div`
-    padding: 0;
-    margin: 0;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  padding: 0;
+  margin: auto;
+  width: calc(100vh / 16 * 9);
+  height: 100vh;
+  position: relative;
 `;
 
-const Heading = styled.h1`
-  margin: 0 auto;
-  padding: 18px;
+const OverlayImage = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
+  object-fit: cover;
+`;
+
+const BackgroundImage = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  object-fit: cover;
 `;
 
 class App extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            overlay: Koji.config.images.overlay,
+            background: Koji.config.images.background,
+        };
+    }
+
+  componentDidMount() {
+    window.addEventListener('KojiPreview.DidChangeVcc', (e) => {
+        try {
+            const {
+                path,
+                newValue,
+            } = e.detail;
+            const key = path[1];
+            this.setState({
+                [key]: newValue,
+            });
+        } catch (err) {
+            //
+        }
+    });
+  }
+
   render() {
     return (
       <Container>
-        <Heading>{Koji.config.strings.heading}</Heading>
+        <OverlayImage src={this.state.overlay} />
+        <BackgroundImage src={this.state.background} />
       </Container>
     );
   }
