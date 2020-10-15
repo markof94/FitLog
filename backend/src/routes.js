05ec31e8e749ef4a268ee82ba28ed7766aecd1a8
gideonaireX <admin@koji-projects.com>
2020-10-15 15:33:00 -0700

    update

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,30 +4,21 @@ import { Keystore } from '@withkoji/vcc';
 
 export default function (app) {
   app.get('/preview', async (req, res) => {
+    console.log('got preview request', Date.now());
     const { image } = res.locals.koji.general;
     
     const keystore = new Keystore();
     const resolvedImage = await keystore.resolveValue(image);
+    console.log('resolved image value', Date.now());
     const blurredImage = `${resolvedImage}?format=jpg&optimize=medium&blur=70`;
 
     res.header('Content-Type', 'image/jpeg');
     const request = await fetch(blurredImage);
+    console.log('fetched image', Date.now());
     request.body.pipe(res);
   });
 
   app.get('/unlocked', async (req, res) => {
-    // The image is stored using the composable secret VCC, so the value
-    // actually written in the file is a keypath that we can query to
-    // receive the actual value
-    const { image } = res.locals.koji.general;
-    
-    // Use the Keystore to resolve the value of the image from its keypath
-    const keystore = new Keystore();
-    const resolvedImage = await keystore.resolveValue(image);
-
-    // Use CDN params to create both the revealed image and the blurred image
-    const revealedImage = `${resolvedImage}?format=jpg&optimize=medium`;
-
     // Use the IAP callback token to see if we can find a receipt matching the SKU of
     // the image. If a receipt exists for the SKU, then we know the user has purchased
     // the image.
@@ -47,11 +38,23 @@ export default function (app) {
     // Set the content type, so the browser knows to display an image
     res.header('Content-Type', 'image/jpeg');
 
-    if (hasPurchased) {
-      const request = await fetch(revealedImage);
-      request.body.pipe(res);
-    } else {
+    if (!hasPurchased) {
       res.sendStatus(401);
+      return;
     }
+
+    // The image is stored using the composable secret VCC, so the value
+    // actually written in the file is a keypath that we can query to
+    // receive the actual value
+    const { image } = res.locals.koji.general;
+    
+    // Use the Keystore to resolve the value of the image from its keypath
+    const keystore = new Keystore();
+    const resolvedImage = await keystore.resolveValue(image);
+
+    // Use CDN params to create both the revealed image and the blurred image
+    const revealedImage = `${resolvedImage}?format=jpg&optimize=medium`;
+    const request = await fetch(revealedImage);
+    request.body.pipe(res);
   });
 }
