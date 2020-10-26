00bbe129a813bbfd9085a4c4bb5e8f4b4007530d
sean <sean@koji-projects.com>
2020-10-26 23:02:08 +0000

    Manual deploy

--- frontend/common/pages/Screenshot.js
+++ frontend/common/pages/Screenshot.js
@@ -22,12 +22,34 @@ const Image = styled.img`
   object-fit: contain;
 `;
 
+const Title = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  z-index: 2;
+  text-shadow: 0px 0px 2px rgba(0,0,0,0.5);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  color: white;
+  font-size: 2rem;
+  padding: 24px;
+`;
+
 class SceneRouter extends React.PureComponent {
   render() {
     const instantRemixing = new InstantRemixing();
+    const image = instantRemixing.get(['general', 'headerImage']);
+    const title = instantRemixing.get(['general', 'title']);
     return (
       <Container>
-        <Image src={`${instantRemixing.get(['serviceMap', 'backend'])}/preview`} />
+        <Image src={image} />
+        <Title>{title}</Title>
       </Container>
     );
   }
